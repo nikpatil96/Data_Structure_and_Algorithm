476f5b29f15492090a156cb1d9c770f4c7c5b24e
Nikita Pawar <nikita96pawar@gmail.com>
2023-07-25 11:12:50 -0700

    Initial commit-Linear Search

---
+++ DataStructureAndAlgorithm/LinearSearch.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureAndAlgorithm
+{
+    public class LinearSearch
+    {
+        public static void Main()
+        {
+            int[] arr = { 10, 30, 12, 11, 323, 345, 566 };
+            int target = -1;
+            // int ans = Linearsearch(arr, target);
+            int[,] arr2D = new int[,] {  { 1, 2, 3 }, { 4, 5, 6 } ,
+                                  { 7, 8, 9 }, { 10, 11, 12 }  };
+            int[] ans = LinearSearch2D(arr2D, target);
+            foreach(int i in ans)
+            {
+                Console.WriteLine(i);
+            }
+          
+            Console.ReadLine();
+        }
+        static int[] LinearSearch2D(int[,] arr, int target)
+        {
+           for(int row=0;row< arr.GetLength(0); row++)
+            {
+                for(int col = 0; col < arr.GetLength(1);col++)
+                {
+                    if (arr[row,col]==target)
+                    {
+                        return new int[] { row, col };
+                    }
+                }          
+            }
+            return  new int[] { -1, -1 };
+        }
+        static int Linearsearch(int[] arr , int target)
+        {
+            foreach(int i in arr)
+            {
+                if(target== i)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ DataStructureAndAlgorithm/Program.cs
@@ -0,0 +1,70 @@
+﻿class Program
+{
+
+    //Wealth prob
+
+    //public int MaximumWealth(int[][] accounts)
+    //{
+    //    int sum = 0;
+    //    int max = 0;
+    //    for (int i = 0; i < accounts.Length; i++)
+    //    {
+    //        for (int j = 0; i < accounts[i].Length; j++)
+    //        {
+    //            sum += accounts[i][j];
+    //        }
+
+    //        if (max == 0)
+    //        {
+    //            max = sum;
+    //        }
+    //        if (max != 0)
+    //        {
+    //            if (max < sum)
+    //            {
+    //                max = sum;
+    //            }
+    //        }
+    //    }
+
+    //    return max;
+    //}
+
+    //public static void Main()
+    //{
+     
+    //    Console.WriteLine("Enter array Length \n");
+    //    int len=Convert.ToInt32(Console.ReadLine());
+    //    int[] nums = new int[len];
+    //    Console.WriteLine("Enter array elements \n");
+    //    for (int i = 0; i < nums.Length; i++)
+    //    {           
+    //        nums[i] = Convert.ToInt32(Console.ReadLine());
+    //    }
+    //    Console.WriteLine("Enter Partition number \n");
+    //    int n = Convert.ToInt32(Console.ReadLine());
+    //    int[] ans = Shuffle(nums, n);
+    //    foreach (int answer in ans)
+    //    {
+    //        Console.WriteLine(answer);
+    //    }
+    //    Console.ReadLine();
+    //}
+    static int[] Shuffle(int[] nums, int n)
+    {
+        int[] ans = new int[nums.Length];
+        int index = 0, index1 = 0;
+    
+        
+        while (index<n && index1<nums.Length-1)
+        {
+
+            Console.WriteLine("Index " + index + "\t" + index1);
+            ans[index1] = nums[index];
+            ans[index1+1] = nums[index + n];
+            index++;
+            index1=index1+2;
+        }
+        return ans;
+    }
+}
